d91076df5fa98702b1601572e2f1afa51d1d994e
Mrudul Dubey <mrudulmad29@gmail.com>
2019-04-06 23:45:53 -0400

    done

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   // "URI" : "mongodb://localhost/portfolio",
     //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
-    "URI":"mongodb://mad:1234@ds044577.mlab.com:44577/mportfolio",
+    "URI":"mongodb://mrudul:mad123@ds044577.mlab.com:44577/mportfolio",
     "secret": "SomeSecret"
 }
